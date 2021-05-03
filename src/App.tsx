a11b6c353c45bdd1fb28a604c9ebf2284f655751
gpbaculio <gpbaculio@gmail.com>
2021-05-03 17:20:32 +0800

    adjust size / add donut chart

--- src/App.tsx
+++ src/App.tsx
@@ -23,48 +23,21 @@ function App() {
         age: "30-40",
         population: 4,
       },
-      {
-        age: "40-50",
-        population: 5,
-      },
-      {
-        age: "50-60",
-        population: 6,
-      },
-      {
-        age: "60-70",
-        population: 7,
-      },
-      {
-        age: "70-80",
-        population: 8,
-      },
-      {
-        age: "80-90",
-        population: 9,
-      },
-      {
-        age: "90-100",
-        population: 10,
-      },
-      {
-        age: "100+",
-        population: 11,
-      },
     ];
 
-    const margin = { top: 30, right: 120, bottom: 30, left: 50 },
+    const margin = { top: 30, right: 30, bottom: 30, left: 50 },
       width = 960 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom,
       tooltip = { width: 100, height: 100, x: 10, y: -30 };
 
+    //pie
     const svg = d3
       .select(".pie")
       .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
+      .attr("width", 300)
+      .attr("height", 300)
       .append("g")
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+      .attr("transform", "translate(" + 300 / 2 + "," + 300 / 2 + ")");
     interface PieData {
       age: string;
       population: number;
@@ -78,17 +51,18 @@ function App() {
     const arc = d3
       .arc<PieArcDatum<PieData>>()
       .innerRadius(0)
-      .outerRadius(Math.min(width, height) / 2 - 1)
-      .cornerRadius(15);
+      .outerRadius(Math.min(300, 300) / 2 - 1)
+      .cornerRadius(12);
 
     const arcLabel = function () {
-      const radius = (Math.min(width, height) / 2) * 0.8;
+      const radius = (Math.min(300, 300) / 2) * 0.8;
       return d3
         .arc<PieArcDatum<PieData>>()
         .innerRadius(radius)
         .outerRadius(radius);
     };
-    var color = d3
+
+    const color = d3
       .scaleOrdinal()
       .domain(data.map((d) => d.age))
       .range(
@@ -96,6 +70,7 @@ function App() {
           .quantize((t) => d3.interpolateSpectral(t * 0.8 + 0.1), data.length)
           .reverse()
       );
+
     const arcs = pie(data);
 
     svg
@@ -109,6 +84,7 @@ function App() {
       .attr("d", arc)
       .append("title")
       .text((d) => `${d.data.age}: ${d.data.population.toLocaleString()}`);
+
     svg
       .append("g")
       .attr("font-family", "sans-serif")
@@ -116,6 +92,70 @@ function App() {
       .attr("text-anchor", "middle")
       .selectAll("text")
       .data(arcs)
+      .enter()
+      .append("text")
+      .attr("transform", (d) => `translate(${arcLabel().centroid(d)})`)
+      .call((text) =>
+        text
+          .append("tspan")
+          .attr("y", "-0.4em")
+          .attr("font-weight", "bold")
+          .text((d) => d.data.age)
+      )
+      .call((text) =>
+        text
+          .filter((d) => d.endAngle - d.startAngle > 0.25)
+          .append("tspan")
+          .attr("x", 0)
+          .attr("y", "0.7em")
+          .attr("fill-opacity", 0.7)
+          .text((d) => d.data.population.toLocaleString())
+      );
+
+    // donut
+    const donutSvg = d3
+      .select(".donut")
+      .append("svg")
+      .attr("width", 300)
+      .attr("height", 300)
+      .append("g")
+      .attr("transform", "translate(" + 300 / 2 + "," + 300 / 2 + ")");
+
+    const donutPie = d3
+      .pie<void, PieData>()
+      .sort(null)
+      .value((d) => d.population);
+
+    const innerRadius = Math.min(300, 300) / 2 - 50;
+    const outerRadius = Math.min(300, 300) / 2 - 1;
+
+    const donutArc = d3
+      .arc<PieArcDatum<PieData>>()
+      .innerRadius(innerRadius)
+      .outerRadius(outerRadius)
+      .cornerRadius(12);
+
+    const donutArcs = donutPie(data);
+
+    donutSvg
+      .append("g")
+      .attr("stroke", "white")
+      .selectAll("path")
+      .data(donutArcs)
+      .enter()
+      .append("path")
+      .attr("fill", (d) => color(d.data.age) as string)
+      .attr("d", donutArc)
+      .append("title")
+      .text((d) => `${d.data.age}: ${d.data.population.toLocaleString()}`);
+
+    donutSvg
+      .append("g")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 12)
+      .attr("text-anchor", "middle")
+      .selectAll("text")
+      .data(donutArcs)
       .enter()
       .append("text")
       .attr("transform", (d) => `translate(${arcLabel().centroid(d)})`)
